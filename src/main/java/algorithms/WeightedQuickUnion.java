a4e0d86cda16029c374b8ca0f41e0b0c258c5c4a
OmarKaempfer <ovkgithub@gmail.com>
2020-09-17 16:06:43 +0100

    Implement Weighted Quick Union

--- src/main/java/algorithms/WeightedQuickUnion.java
+++ src/main/java/algorithms/WeightedQuickUnion.java
@@ -1,14 +1,17 @@
 package algorithms;
 
-public class QuickUnion {
+public class WeightedQuickUnion {
 
     private final int[] elements;
+    private final int[] treeSizes;
 
-    public QuickUnion(int size) {
+    public WeightedQuickUnion(int size) {
 
         elements = new int[size];
+        treeSizes = new int[size];
         for (int i = 0; i < size; i++) {
             elements[i] = i;
+            treeSizes[i] = 1;
         }
     }
 
@@ -17,14 +20,23 @@ public class QuickUnion {
      * @param firstElement
      * @param secondElement
      *
-     * Worst case time complexity: 2N -> N
+     * Worst case time complexity: 2 log N -> log N
      */
     public void connect(int firstElement, int secondElement) {
 
         int firstRoot = findRoot(firstElement);
         int secondRoot = findRoot(secondElement);
+        if(firstRoot == secondRoot) {
+            return;
+        }
 
-        elements[firstRoot] = secondRoot;
+        if(treeSizes[firstRoot] >= treeSizes[secondRoot]) {
+            elements[secondRoot] = firstRoot;
+            treeSizes[firstRoot] += treeSizes[secondRoot];
+        } else {
+            elements[firstRoot] = secondRoot;
+            treeSizes[secondRoot] += treeSizes[firstRoot];
+        }
     }
 
 
@@ -32,7 +44,7 @@ public class QuickUnion {
      * @param firstElement
      * @param secondElement
      *
-     * Worst case time complexity: 2 N -> N
+     * Worst case time complexity: 2 log N -> log N
      * @return
      */
     public boolean connected(int firstElement, int secondElement) {
@@ -43,8 +55,8 @@ public class QuickUnion {
     /**
      * @param element
      *
-     * Worst case time complexity: N
-     * When the trees are too tall
+     * Worst case time complexity: log N
+     * Height is reduced dramatically by using weighted trees
      * @return
      */
     private int findRoot(int element) {
