7b8795803b202137da96e43e7dd3aed0cbda7ae4
OmarKaempfer <ovkgithub@gmail.com>
2020-09-17 12:23:15 +0100

    Implement Quick Find

---
+++ src/test/java/algorithms/QuickFindTest.java
@@ -0,0 +1,34 @@
+package algorithms;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class QuickFindTest {
+
+    @Test
+    public void connect() {
+
+        QuickFind quickFind = new QuickFind(10);
+        assertThat(quickFind.connected(1, 2)).isFalse();
+        quickFind.connect(1, 2);
+        assertThat(quickFind.connected(1, 2)).isTrue();
+    }
+
+    @Test
+    public void anElementShouldBeConnectedToItself() {
+        QuickFind quickFind = new QuickFind(10);
+        assertThat(quickFind.connected(0, 0)).isTrue();
+    }
+
+    @Test
+    public void shouldMeetTheTransitiveProperty() {
+
+        QuickFind quickFind = new QuickFind(10);
+        assertThat(quickFind.connected(0, 2)).isFalse();
+
+        quickFind.connect(0, 1);
+        quickFind.connect(1, 2);
+        assertThat(quickFind.connected(0, 2)).isTrue();
+    }
+}
